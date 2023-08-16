cff668b492c539e4ac52b595576752e8696b1b7c
andres.mera <feli041089@gmail.com>
2023-08-16 10:39:30 -0500

    Calculando el total con un Computed Property

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { computed } from 'vue';
+
 const props = defineProps({
     carrito: {
         type: Array,
@@ -10,7 +12,11 @@ const props = defineProps({
     }
 });
 
-defineEmits(['decrementar-cantidad', 'incrementar-cantidad', 'agregar-carrito'])
+defineEmits(['decrementar-cantidad', 'incrementar-cantidad', 'agregar-carrito']);
+
+const totalPagar = computed(() => {
+    return props.carrito.reduce((total, producto) => total + (producto.cantidad * producto.precio), 0)
+});
 </script>
 <template>
     <header class="py-5 header">
@@ -68,7 +74,7 @@ defineEmits(['decrementar-cantidad', 'incrementar-cantidad', 'agregar-carrito'])
                                     </tbody>
                                 </table>
 
-                                <p class="text-end">Total pagar: <span class="fw-bold">$899</span></p>
+                                <p class="text-end">Total pagar: <span class="fw-bold">${{ totalPagar }}</span></p>
                                 <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                             </div>
                         </div>
